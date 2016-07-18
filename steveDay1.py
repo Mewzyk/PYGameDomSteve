a941bee94cb3bb02f3a78351acd30d2a1c5a773b
Mewzyk <Mewzyk@gmail.com>
2016-07-17 20:52:51 -0700

    PyGame first stuff

---
+++ steveDay1.py
@@ -0,0 +1,18 @@
+import pygame
+import random
+
+screen = pygame.display.set_mode((640,400))
+running = 1
+
+while running:
+	event = pygame.event.poll()
+	if event.type == pygame.QUIT:
+		running = 0
+	screen.fill((255,255,255))
+	pygame.display.flip()
+
+	pygame.draw.line(screen, (0,0,0), (random.randint(0,255),0), (639,479))
+	pygame.draw.aaline(screen, (0,0,0), (639,0), (0,479))
+
+	pygame.display.flip()
+
